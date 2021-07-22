8f61813702bb80bc1a8a8550aaab9dd7b49a30ec
Mcruz92 <81129725+Mcruz92@users.noreply.github.com>
2021-07-21 23:19:07 -0400

    Added the Github Repo URL

--- src/App.js
+++ src/App.js
@@ -2,7 +2,20 @@ import React from "react"
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+   <div className="App">
+    <h1>Weather App</h1>
+     <footer>
+      This project was coded by Michele Ashley Cruz and is{" "}
+    <a 
+      href="https://github.com/Mcruz92/react-weather-app1"
+      target="_blank"
+      >
+        open-sourced on Github
+       </a>
+     </footer>
+    </div>
+  );
 }
 
  

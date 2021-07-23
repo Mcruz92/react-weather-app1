11fd9573f60572cf12fac5b7820864eab6ea8dce
Mcruz92 <81129725+Mcruz92@users.noreply.github.com>
2021-07-22 23:01:16 -0400

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -1,19 +1,27 @@
 import React from "react"
+
 import './App.css';
 
 export default function App() {
   return (
    <div className="App">
+    <div className="container">
+
     <h1>Weather App</h1>
      <footer>
-      This project was coded by Michele Ashley Cruz and is{" "}
+      This project was coded by{" "}
+      <a href="https://www.linkedin.com/in/michele-cruz-86554b86/" target="_blank">
+       Michele Ashley Cruz 
+      </a>{" "}
+       and is{" "}
     <a 
-      href="https://github.com/Mcruz92/react-weather-app1"
-      target="_blank"
+       href="https://github.com/Mcruz92/react-weather-app1"
+       target="_blank"
       >
         open-sourced on Github
        </a>
      </footer>
+     </div>
     </div>
   );
 }

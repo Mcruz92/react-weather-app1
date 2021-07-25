1f2984cb576592e23f9a200801e0115a4c3025a6
Mcruz92 <81129725+Mcruz92@users.noreply.github.com>
2021-07-24 23:44:28 -0400

    Added API Integration

--- src/App.js
+++ src/App.js
@@ -1,29 +1,23 @@
-import React from "react"
-
-import './App.css';
+import React from "react";
+import Weather from "./Weather";
+import "./App.css";
 
 export default function App() {
   return (
-   <div className="App">
-    <div className="container">
-
-    <h1>Weather App</h1>
-     <footer>
-      This project was coded by{" "}
-      <a href="https://www.linkedin.com/in/michele-cruz-86554b86/" target="_blank">
-       Michele Ashley Cruz 
-      </a>{" "}
-       and is{" "}
-    <a 
-       href="https://github.com/Mcruz92/react-weather-app1"
-       target="_blank"
-      >
-        open-sourced on Github
-       </a>
-     </footer>
-     </div>
+    <div className="App">
+      <div className="container">
+        <Weather defaultCity="Orlando" />
+        <p className="text-center">
+          <a
+            href="https://github.com/Mcruz92/react-weather-app1"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Open-sourced on GitHub{" "}
+          </a>
+          by Michele Ashley Cruz
+        </p>
+      </div>
     </div>
   );
 }
-
- 
